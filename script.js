4813101ca4cc3c1c43a386598be5137235554923
cert-ctf <91723918+cert-ctf@users.noreply.github.com>
2021-12-06 20:34:41 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-WA.room.onEnterLayer('door_open').subscribe(() => {
-	WA.room.hideLayer('gateopen');
-	WA.room.showLayer('gateclosed');
+//Init
+WA.onInit().then(() => {
+    WA.room.hideLayer('gate_open');
 });
 
+//Open Door
+WA.room.onEnterLayer('door_open').subscribe(() => {
+	WA.room.hideLayer('gate_open');
+	WA.room.showLayer('gate_closed');
+});
+
+//Close Door
 WA.room.onEnterLayer('door_close').subscribe(() => {
-	WA.room.hideLayer('gateopen');
-	WA.room.showLayer('gateclosed');
+	WA.room.hideLayer('gate_open');
+	WA.room.showLayer('gate_closed');
 });
