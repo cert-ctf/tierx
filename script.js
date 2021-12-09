a9d44a53c0a03a4e3cf16a07b76083b1ed9110c4
cert-ctf <91723918+cert-ctf@users.noreply.github.com>
2021-12-09 12:16:26 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,21 +2,3 @@
 WA.onInit().then(() => {
     WA.room.hideLayer('gate_open');
 });
-
-//Open Door
-WA.room.onEnterLayer('door_open').subscribe(() => {
-	if (WA.player.tags.includes('admin'))
-	{
-		WA.room.showLayer('gate_open');
-		WA.room.hideLayer('gate_closed');
-	}
-});
-
-//Close Door
-WA.room.onEnterLayer('door_close').subscribe(() => {
-	if (WA.player.tags.includes('admin'))
-	{
-		WA.room.hideLayer('gate_open');
-		WA.room.showLayer('gate_closed');
-	}
-});
