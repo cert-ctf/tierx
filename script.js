39305e07d7b1e8a04bcd62c8c115524aaf912257
cert-ctf <91723918+cert-ctf@users.noreply.github.com>
2021-12-09 12:45:16 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-(async () => {
-    await WA.onInit();
-    console.log('Current player name: ', WA.player.name);
-	WA.room.hideLayer('gate_open');
-})();
+//Init
+WA.onInit().then(() => {
+    WA.room.hideLayer('gate_open');
+});
+
+//Open Door
+WA.room.onEnterLayer('door_open').subscribe(() => {
+	if (WA.player.tags.includes('admin'))
+	{
+		WA.room.showLayer('gate_open');
+		WA.room.hideLayer('gate_closed');
+	}
+});
+
+//Close Door
+WA.room.onEnterLayer('door_close').subscribe(() => {
+	if (WA.player.tags.includes('admin'))
+	{
+		WA.room.hideLayer('gate_open');
+		WA.room.showLayer('gate_closed');
+	}
+});
